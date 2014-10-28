10e75c762132d3354de8f863c58d40692a81d511
Marshall Norris <devmarshallnorris@gmail.com>
2014-10-28 19:00:10 +0000

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -32,9 +32,11 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
         
-        table = self.browser.find_elements_by_tag_name('tr')
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # Still a text box, enter "Use peacock feathers to make a fly"
