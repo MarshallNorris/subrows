5ff1432bb55ef0890dfa575ff744a0021eb7ea12
Marshall Norris <devmarshallnorris@gmail.com>
2014-10-28 20:14:23 +0000

    Site can handle list of 1 item (no more).

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -34,17 +35,23 @@ class NewVisitorTest(unittest.TestCase):
         
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # Still a text box, enter "Use peacock feathers to make a fly"
-        self.fail('Finish the test!')
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        
         # Page updates and shows both items
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly' ,
+                      [row.text for row in rows]
+        )
+        
         # Explanatory text about remembering users with a unique URL
+        self.fail('Finish the test!') 
          
         # Click on URL, to-do list still there
          
