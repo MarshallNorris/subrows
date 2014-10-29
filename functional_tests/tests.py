af972642a9e7c3c64c76276e238c4cdb6f745f80
Marshall Norris <devmarshallnorris@gmail.com>
2014-10-29 16:41:47 +0000

    First steps of FT for layour + style.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -75,6 +75,27 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-
+        
+    def test_layout_and_styling(self):
+        # Open home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        
+        # Is inputbox centred
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 
+            512,
+            delta=5
+        )
+        
+        # Submit a new list is inputbox centred
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 
+            512,
+            delta=5
+        )
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
