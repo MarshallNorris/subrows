51fba01071ad2c876bca3bf7773e3c19c92f45fe
Marshall Norris <devmarshallnorris@gmail.com>
2014-11-13 21:45:41 +0000

    Added FT for removing error message

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -9,7 +9,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('\n')
         
         # Error message appears, item cannot be blank
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
         
         # User enters correct item
@@ -21,7 +21,7 @@ class ItemValidationTest(FunctionalTest):
         
         # Similar warning appears
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
         
         # User fills some text in
@@ -40,5 +40,22 @@ class ItemValidationTest(FunctionalTest):
         
         # Error message appears
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
+        
+    def test_error_messages_are_cleared_on_input(self):
+        # User start a new list that causes validation error
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+        
+        # User starts typing into input box to clear the error
+        self.get_item_input_box().send_keys('a')
+        
+        # Error message disappears
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
+        
+    def get_error_element(self):    
+        return self.browser.find_element_by_css_selector('.has-error')
